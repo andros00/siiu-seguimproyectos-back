208fdf36e1dc145b87bc1dc010242aeea226e68a
andres.tirado <andres.tirado@udea.edu.co>
2025-09-16 16:11:25 -0500

    Implementación de dependencias swagger para documentación

--- src/main/java/seguimproyectos_back/configuration/SwaggerConfig.java
+++ src/main/java/seguimproyectos_back/configuration/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package seguimproyectos_back.configuration;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,7 +21,19 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.apis(RequestHandlerSelectors.basePackage("seguimproyectos_back.controller")).paths(PathSelectors.any())
-				.build();
+				.build().apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	            "siiu-seguimproyectos-back",
+	            "Sistema de información de investigación universitaria",
+	            "1.0",
+	            "Términos de servicio",
+	            new Contact("desarrollo.siiu1", "https://udea.edu.co", "desarrollo.siiu1@udea.edu.co"),
+	            "Licencia",
+	            "URL de licencia",
+	            Collections.emptyList());
 	}
 
 }
--- src/main/java/seguimproyectos_back/controller/ConsultaProyectosController.java
+++ src/main/java/seguimproyectos_back/controller/ConsultaProyectosController.java
@@ -1,6 +1,5 @@
 package seguimproyectos_back.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,17 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.udea.utilities.exception.UdeaException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import seguimproyectos_back.bl.interfaces.ConsultaProyectosService;
 import seguimproyectos_back.model.GenericResponse;
 import seguimproyectos_back.model.Proyecto;
 
 @RestController
 @RequestMapping("proyectos")
+@Api(value = "Proyectos", tags = { "Gestión de proyectos" })
 public class ConsultaProyectosController {
 
 	private final ConsultaProyectosService consultaProyectosService;
 
-	@Autowired
 	public ConsultaProyectosController(ConsultaProyectosService consultaProyectosService) {
 		this.consultaProyectosService = consultaProyectosService;
 	}
@@ -28,7 +31,11 @@ public class ConsultaProyectosController {
 	public String obteneVariableCertificado() {
 		return "Hola mundo";
 	}
-	
+
+	@ApiOperation(value = "Buscar proyectos por según los filtros suministrados", notes = "Provee uno o varios proyectos específico")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "proyecto encontrado"),
+			@ApiResponse(code = 404, message = "proyecto no encontrado") })
+
 	@PostMapping(value = "/consultar")
 	public ResponseEntity<GenericResponse> consultar(@RequestBody Proyecto proyecto) throws UdeaException {
 		GenericResponse genericResponse = new GenericResponse();
